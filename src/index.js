419bead5745a8c5481d00a0c6d03c97038c35f54
Adam Zhang <adamorsomething@protonmail.com>
2020-10-01 10:49:44 -0400

    stuff

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Dictionary = ({ words }) => (
+  <div>
+    <Title />
+    <Subtitle />
+    <Table words={words} />
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Title = () => (
+  <h1>Dictionary</h1>
+);
+
+const Subtitle = () => (
+  <h2>Written by me, researched by me, published by me.</h2>
+);
+
+const Table = ({ words }) => (
+  <table>
+    <tr>
+      <th>Word</th>
+      <th>Part of Speech</th>
+      <th>Definition</th>
+    </tr>
+    {words.map(word => {
+      return <Word word={word} />
+    })}
+  </table>
+);
+
+const Word = ({ word }) => (
+  <tr>
+    <td>{word.word}</td>
+    <td>{word.partOfSpeech}</td>
+    <td>{word.definition}</td>
+  </tr>
+);
+
+const PARTS_OF_SPEECH = {
+  ADJECTIVE: 'adjective',
+  PREFIX: 'prefix'
+};
+
+const words = [
+  {
+    word: 'veldt-y',
+    partOfSpeech: PARTS_OF_SPEECH.ADJECTIVE,
+    definition: 'Exhibiting properties of a veldt.'
+  },
+  {
+    word: 'col-',
+    partOfSpeech: PARTS_OF_SPEECH.PREFIX,
+    definition: 'The opposite of row.'
+  }
+];
+
+ReactDOM.render(
+  <Dictionary words={words} />,
+  document.querySelector('#root')
+);
