57306b960c34a77e5c0617be75d7660e0d726dfd
ADam <adamorsomething@protonmail.com>
2020-10-20 12:50:28 -0400

    hyperadam

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 const Dictionary = ({ words }) => (
   <div>
@@ -10,9 +10,7 @@ const Dictionary = ({ words }) => (
   </div>
 );
 
-const Title = () => (
-  <h1>Dictionary</h1>
-);
+const Title = () => <h1>Dictionary</h1>;
 
 const Subtitle = () => (
   <h2>Written by me, researched by me, published by me.</h2>
@@ -25,8 +23,8 @@ const Table = ({ words }) => (
       <th>Part of Speech</th>
       <th>Definition</th>
     </tr>
-    {words.map(word => {
-      return <Word word={word} />
+    {words.map((word) => {
+      return <Word word={word} />;
     })}
   </table>
 );
@@ -40,35 +38,38 @@ const Word = ({ word }) => (
 );
 
 const PARTS_OF_SPEECH = {
-  ADJECTIVE: 'adjective',
-  PREFIX: 'prefix',
-  NOUN: 'noun'
+  ADJECTIVE: "adjective",
+  PREFIX: "prefix",
+  NOUN: "noun",
 };
 
 const words = [
   {
-    word: 'veldt-y',
+    word: "veldt-y",
     partOfSpeech: PARTS_OF_SPEECH.ADJECTIVE,
-    definition: 'Exhibiting properties of a veldt.'
+    definition: "Exhibiting properties of a veldt.",
   },
   {
-    word: 'col-',
+    word: "col-",
     partOfSpeech: PARTS_OF_SPEECH.PREFIX,
-    definition: 'The opposite of row.'
+    definition: "The opposite of row.",
   },
   {
-    word: 'burriwich',
+    word: "burriwich",
     partOfSpeech: PARTS_OF_SPEECH.NOUN,
-    definition: 'A sandwich made with tortilla instead of bread.'
+    definition: "A sandwich made with tortilla instead of bread.",
   },
   {
-    word: 'adamword',
+    word: "adamword",
     partOfSpeech: PARTS_OF_SPEECH.NOUN,
-    definition: 'A word found in this dictionary.'
-  }
+    definition: "A word found in this dictionary.",
+  },
+  {
+    word: "hyperadam",
+    partOfSpeech: PARTS_OF_SPEECH.NOUN,
+    definition:
+      "When you have a blast of hyper thoughts that only Adam can have. - Courtesy of Reja",
+  },
 ];
 
-ReactDOM.render(
-  <Dictionary words={words} />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Dictionary words={words} />, document.querySelector("#root"));
